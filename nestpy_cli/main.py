5f65170c984925cd9e76f51d506962e69cb12042
Manuel S. Lemos <manuelslemos@gmail.com>
2024-01-17 20:56:28 +0000

    implement --version

--- nestpy_cli/main.py
+++ nestpy_cli/main.py
@@ -1,26 +1,41 @@
+from importlib import metadata
+
 import typer
 
 
 app = typer.Typer()
+__version__ = metadata.version(__package__)
 
+def version_callback(value: bool) -> None:
+    if value:
+        print(__version__)
+        raise typer.Exit()
 
 @app.callback()
-def callback():
+def callback(
+    version: bool = typer.Option(None, "--version", "-v", callback=version_callback)
+) -> None:
     """
-    Awesome Portal Gun
+    Nestpy CLI Application
     """
 
 
-@app.command()
-def shoot():
+@app.command('new')
+@app.command('n')
+def new(
+    name: str,
+    skip_git: bool = typer.Option(None, '--skip-git', '-g'),
+    skip_install: bool = typer.Option(None, '--install', '-g'),
+):
     """
     Shoot the portal gun
     """
     typer.echo("Shooting portal gun")
 
 
-@app.command()
-def load():
+@app.command('generate')
+@app.command('g')
+def generate():
     """
     Load the portal gun
     """
